f2c269eae1f332d7b024178d37399228b9b11b12
byy0643 <classhero20105@gmail.com>
2023-11-22 10:11:07 +0900

    Additional Motion and Player UI

--- Source/ADVProject/private/ADVPlayer.cpp
+++ Source/ADVProject/private/ADVPlayer.cpp
@@ -5,29 +5,56 @@
 #include <GameFramework/SpringArmComponent.h>
 #include <Camera/CameraComponent.h>
 #include <GameFramework/CharacterMovementComponent.h>
+#include "PlayerAnim.h"
+#include "Arrow.h"
 // Sets default values
 AADVPlayer::AADVPlayer()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	ConstructorHelpers::FObjectFinder<USkeletalMesh> TempMesh(TEXT("SkeletalMesh'/Game/Characters/Erika_Archer.Erika_Archer'"));
-	if (TempMesh.Succeeded()) {
-		GetMesh()->SetSkeletalMesh(TempMesh.Object);
-		GetMesh()->SetRelativeLocationAndRotation(FVector(0, 0, 90), FRotator(0, -90, 0));
+	ConstructorHelpers::FObjectFinder<USkeletalMesh> tempMesh(TEXT("SkeletalMesh'/Game/Characters/Erika_Archer.Erika_Archer'"));
+	if (tempMesh.Succeeded()) {
+		GetMesh()->SetSkeletalMesh(tempMesh.Object);
+		GetMesh()->SetRelativeLocationAndRotation(FVector(0, 0, -90), FRotator(0, -90, 0));
 	}
 
 	springArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
 	springArmComp->SetupAttachment(RootComponent);
-	springArmComp->SetRelativeLocation(FVector(0, 70, 90));
+	springArmComp->SetRelativeLocation(FVector(0, 0, 90));
 	springArmComp->TargetArmLength = 400;
 	springArmComp->bUsePawnControlRotation = true;
 
 	tpsCamComp = CreateDefaultSubobject<UCameraComponent>(TEXT("TpsCamComp"));
 	tpsCamComp->SetupAttachment(springArmComp);
-	tpsCamComp->bUsePawnControlRotation = false;
 
-	bUseControllerRotationYaw = true;
+
+	bUseControllerRotationYaw = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+
+	JumpMaxCount = 1;
+
+	unarmedBowMeshComp = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("UnarmedBowMeshComp"));
+	unarmedBowMeshComp->SetupAttachment(GetMesh(), TEXT("Spine2Socket"));
+	ConstructorHelpers::FObjectFinder<USkeletalMesh> TempUnarmedBowMesh(TEXT("/Script/Engine.SkeletalMesh'/Game/Props/Bow.Bow'"));
+
+	if (TempUnarmedBowMesh.Succeeded()) {
+		unarmedBowMeshComp->SetSkeletalMesh(TempUnarmedBowMesh.Object);
+		unarmedBowMeshComp->SetRelativeLocation(FVector(10.72, 0, -18.72));
+		unarmedBowMeshComp->SetRelativeRotation(FRotator(-11.23, -183.68, 80.30));
+	}
+
+	armedBowMeshComp = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("ArmedBowMeshComp"));
+	armedBowMeshComp->SetupAttachment(GetMesh(), TEXT("LeftHandSocket"));
+	ConstructorHelpers::FObjectFinder<USkeletalMesh> TempArmedBowMesh(TEXT("/Script/Engine.SkeletalMesh'/Game/Props/Bow.Bow'"));
+
+	if (TempArmedBowMesh.Succeeded()) {
+		armedBowMeshComp->SetSkeletalMesh(TempArmedBowMesh.Object);
+		armedBowMeshComp->SetRelativeLocation(FVector(1.155857, -5.897994, 2.570458));
+		armedBowMeshComp->SetRelativeRotation(FRotator(12.08, 97.60, 66.56));
+		armedBowMeshComp->SetVisibility(false);
+	}
 }
 
 // Called when the game starts or when spawned
@@ -55,8 +82,14 @@ void AADVPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent
 	PlayerInputComponent->BindAxis(TEXT("LookHorizontal"), this, &AADVPlayer::LookHorizontal);
 	PlayerInputComponent->BindAxis(TEXT("MoveVertical"), this, &AADVPlayer::MoveVertical);
 	PlayerInputComponent->BindAxis(TEXT("MoveHorizontal"), this, &AADVPlayer::MoveHorizontal);
+	PlayerInputComponent->BindAction(TEXT("Jump"), IE_Pressed, this, &AADVPlayer::InputJump);
 	PlayerInputComponent->BindAction(TEXT("Run"), IE_Pressed, this, &AADVPlayer::InputRun);
 	PlayerInputComponent->BindAction(TEXT("Run"), IE_Released, this, &AADVPlayer::InputRun);
+	PlayerInputComponent->BindAction(TEXT("Equip"), IE_Pressed, this, &AADVPlayer::InputEquip);
+	PlayerInputComponent->BindAction(TEXT("AimandShoot"), IE_Pressed, this, &AADVPlayer::InputAim);
+	PlayerInputComponent->BindAction(TEXT("AimandShoot"), IE_Released, this, &AADVPlayer::InputShoot);
+	
+
 }
 
 void AADVPlayer::LookVertical(float value) {
@@ -72,6 +105,10 @@ void AADVPlayer::MoveHorizontal(float value) {
 	direction.Y = value;
 }
 
+void AADVPlayer::InputJump() {
+	Jump();
+}
+
 void AADVPlayer::InputRun() {
 	auto movement = GetCharacterMovement();
 
@@ -81,4 +118,52 @@ void AADVPlayer::InputRun() {
 	else {
 		movement->MaxWalkSpeed = runSpeed;
 	}
+}
+
+void AADVPlayer::InputEquip() {
+	auto movement = GetCharacterMovement();
+
+	if (!bArmed) {
+		bArmed = true;
+		unarmedBowMeshComp->SetVisibility(false);
+		armedBowMeshComp->SetVisibility(true);
+	}
+	else {
+		bArmed = false;
+		unarmedBowMeshComp->SetVisibility(true);
+		armedBowMeshComp->SetVisibility(false);
+	}
+
+	if (movement->bOrientRotationToMovement)
+		movement->bOrientRotationToMovement = false;
+	else
+		movement->bOrientRotationToMovement = true;
+
+	if (bUseControllerRotationYaw)
+		bUseControllerRotationYaw = false;
+	else
+		bUseControllerRotationYaw = true;
+}
+
+void AADVPlayer::InputAim() {
+	if (!bArmed) {
+		return;
+	}
+
+	bBowAim = true;
+	springArmComp->SetRelativeLocation(FVector(0, 70, 90));
+	tpsCamComp->SetFieldOfView(45.0f);
+
+
+}
+
+void AADVPlayer::InputShoot() {
+	if (!bArmed) {
+		return;
+	}
+	FTransform shootPosition = armedBowMeshComp->GetSocketTransform(TEXT("ShootPosition"));
+	GetWorld()->SpawnActor<AArrow>(arrowFactory, shootPosition);
+	bBowAim = false;
+	springArmComp->SetRelativeLocation(FVector(0, 0, 90));
+	tpsCamComp->SetFieldOfView(90.0f);
 }
